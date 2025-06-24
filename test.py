fb76073246fd635f0d42dd50ce832097a5a46600
sidiq20 <sidiqolasode@gmail.com>
2025-06-24 03:13:31 +0100

    new chnages

--- test.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-try:
-    client = MongoClient("mongodb+srv://sidiqolasode:8khpTX3g8Wc3tg9f@eviladvice.sszqwnn.mongodb.net/?retryWrites=true&w=majority&appName=eviladvice")
-    client.admin.command("ping")
-    print("✅ MongoDB is connected!")
-except Exception as e:
-    print("❌ MongoDB not connected:", e)
